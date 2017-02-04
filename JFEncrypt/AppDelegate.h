177a2a9ed79fbdfe97dd08d32602e726da59d955
Jeffrey <1441889663@qq.com>
2017-02-04 09:59:31 +0800

    Initial Commit

---
+++ JFEncrypt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFEncrypt
+//
+//  Created by JiangfanZhan on 2017/2/4.
+//  Copyright © 2017年 Jeffrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JFEncrypt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JFEncrypt
+//
+//  Created by JiangfanZhan on 2017/2/4.
+//  Copyright © 2017年 Jeffrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
